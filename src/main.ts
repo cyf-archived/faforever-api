1d9a7d01253ace1bcce4f06fc1637b878004a14b
rojer <rojerchen@qq.com>
2021-12-02 13:40:50 +0800

    feat: :boom: 跨域

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // 开启跨域
+  app.enableCors();
+
   await app.listen(3001);
 }
 bootstrap();
